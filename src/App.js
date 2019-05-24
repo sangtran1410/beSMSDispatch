658964877aee90b8764583f7dc9e0bd5a4871a10
Sang Tran <sang.tran@be.xyz>
2019-05-24 12:00:48 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: ''
+    }
+  }
+
+  onChangeInput = (e) => {
+    const { value } = e.target
+
+    this.setState({ value })
+  }
+
+  submit = () => {
+    console.log(this.state.value)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          beSMSDispatch
+        </header>
+        <content>
+        
+        </content>
+        <footer>
+          <div>
+            <input
+              type="text"
+              onChange={this.onChangeInput}  
+              value={this.state.value}
+              placeholder='Please in put message ...'
+            />
+            <button
+              onClick={this.submit}
+            >
+              Send
+            </button>
+          </div>
+        </footer>
+      </div>
+    );
+  }
 }
 
 export default App;
